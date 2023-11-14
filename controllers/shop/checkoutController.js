6c599a8b95deafa92fc9483fc14e25e8060b774e
Asish <ashisish1858@gamil.com>
2023-11-14 18:27:25 +0530

    edit address , add address ,saved address page completed

--- controllers/shop/checkoutController.js
+++ controllers/shop/checkoutController.js
@@ -2,6 +2,10 @@ const Cart = require("../../models/cartModel");
 const Product = require("../../models/productModel");
 const User = require("../../models/userModels");
 const asynchandler = require("express-async-handler");
+const checkoutHelper=require("../../helpers/checkoutHelpers")
+const {calculateCartTotals}=require("../../helpers/cartHelpers")
+
+
 
 
 
@@ -9,8 +13,17 @@ const asynchandler = require("express-async-handler");
 
 const cartPage=asynchandler(async(req,res)=>{
     try {
-        const address=0
-        res.render('./user/pages/checkout',{title:'WATCHBOX',address})
+        const userid = req.user._id;
+        const user = await User.findById(userid).populate("addresses");
+        cartItems= await checkoutHelper.getCartItems(userid)
+
+        if(cartItems){
+            console.log(cartItems.products);
+            const {subtotal,total}=calculateCartTotals(cartItems.products);
+            res.render('./user/pages/checkout',{title:'WATCHBOX',address:user.addresses,product:cartItems.products ,total,subtotal})
+           
+        }
+ 
         
     } catch (error) {
         throw new Error(error)
